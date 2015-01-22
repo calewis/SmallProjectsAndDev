77aa3af59f1e51bc02c95251363e456acd25be48
drewlewis <drew90@vt.edu>
2015-01-22 10:37:34 -0500

    Removed unneeded headers from problem_3.cpp and made minor improvement.

--- project_euler/cpp/problem_3.cpp
+++ project_euler/cpp/problem_3.cpp
@@ -5,28 +5,24 @@
 #include <chrono>
 #include <iostream>
 #include <vector>
-#include <array>
-#include <algorithm>
-#include <utility>
 #include <cmath>
-
-unsigned long sqrt_plus1(unsigned long input) { return std::sqrt(input) + 1; }
 
 unsigned long problem_3(unsigned long input) {
     std::vector<unsigned long> primes;
+    primes.reserve(10); // Assume small number of primes
 
-    // Don't check numbers above sqrt(num) + 1 because there aren't any
-    // divisors larger than.
-    const auto limit = sqrt_plus1(input);
+    const auto limit = std::sqrt(input) + 1;
     for (auto i = 2ul; i < limit; ++i) {
         if (input % i == 0) {
-            // Divide i by all smaller primes to test for primeyness
+            // Divide i by all smaller primes of input to test for primeyness
             bool prime = true;
             for (auto const &p : primes) {
                 if (i % p == 0) {
                     prime = false;
+                    break;
                 }
             }
+
             if (prime) {  // if i was prime lets add it to our list of primes
                 primes.push_back(i);
             }
